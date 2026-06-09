fe58130365615e9ccb1667af14a3f49f841da0ca
hmackiernan <hmackiernan@perforce.com>
2026-06-08 22:02:43 -0400

    modified-button-behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 
+const greeting: String = "o";
+
+
 function App() {
     const [count, setCount] = useState(0)
-
+    const repeatedGreeting: String = greeting.repeat(count);
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>  Hello{repeatedGreeting}  World</h1>
 
       <button onClick={() => setCount(count + 1)}>
       Clicked {count} times
