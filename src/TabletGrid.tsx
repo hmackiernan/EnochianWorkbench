c326acc46b697a7aaece0d1bdbd532fdbcd415c1
h <h@Jocasta.fios-router.home>
2026-06-20 21:01:16 -0400

    added state to TabletGrid.tsx to remember which cells have been clicked

--- src/TabletGrid.tsx
+++ src/TabletGrid.tsx
@@ -1,10 +1,25 @@
 /* This is the component for the table */
 
+import { useState } from 'react' 
+
+
 function TabletGrid({letters}: { letters: string[][]}) {
   
+    const [selected, setSelected] = useState<Set<string>>(new Set())
+
     const handleCellClick = (r: number, c: number) => {
-    console.log(r, c)
-  }
+  const key = `${r}-${c}`
+  setSelected(prev => {
+    const next = new Set(prev)
+    if (next.has(key)) {
+      next.delete(key)
+    } else {
+      next.add(key)
+    }
+    console.log(key)
+    return next
+  })
+}
 
   
   return (
@@ -13,8 +28,11 @@ function TabletGrid({letters}: { letters: string[][]}) {
         <div key={r} style={{ display: 'flex' }}>
 
           {row.map((letter, c) => (
-<div key={`${r}-${c}`} onClick={() => handleCellClick(r, c)} style={{ border: '1px solid white' }}>
-              {letter}
+<div key={`${r}-${c}`} onClick={() => handleCellClick(r, c)} style={{ 
+  border: '1px solid white',
+  backgroundColor: selected.has(`${r}-${c}`) ? 'yellow' : 'transparent'
+}}>
+ {letter}
             </div>
           ))}
         </div>
